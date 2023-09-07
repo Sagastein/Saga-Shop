f42deaf012cf0190f4ebcd4e67be3e0a804155b8
Sagastein <ishimwe.sage.3@gmail.com>
2023-09-07 19:12:22 +0200

    adding cart provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Navbar from "./components/navbar";
 import Detail from "./pages/detail";
+import { CartProvider } from "./hooks/CartContext";
+
 function App() {
   return (
-    <>
+    <CartProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
+        <Route path="*" element={<div>No Found Route</div>} />
       </Routes>
-    </>
+    </CartProvider>
   );
 }
 
