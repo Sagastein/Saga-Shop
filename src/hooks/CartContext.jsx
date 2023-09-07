818b44e59ba869ce62ad1a8d9925b788db2c5508
Sagastein <ishimwe.sage.3@gmail.com>
2023-09-07 19:14:45 +0200

    cart context hold car count and items

---
+++ src/hooks/CartContext.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+// CartContext.js
+import { createContext, useContext, useState, useEffect } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const totalCount = cartItems.reduce(
+      (count, item) => count + item.quantity,
+      0
+    );
+    setCartCount(totalCount);
+  }, [cartItems]);
+
+  const [cartCount, setCartCount] = useState(0);
+
+  const addToCart = (item) => {
+    const existingItemIndex = cartItems.findIndex(
+      (cartItem) => cartItem.id === item.id
+    );
+
+    if (existingItemIndex !== -1) {
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[existingItemIndex].quantity += item.quantity;
+      setCartItems(updatedCartItems);
+    } else {
+      setCartItems([...cartItems, item]);
+    }
+  };
+
+  return (
+    <CartContext.Provider value={{ cartCount, cartItems, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+}
