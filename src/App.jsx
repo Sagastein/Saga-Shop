dd4bc9ea952274d5cb2cadefb36154be299f6fbc
Sagastein <ishimwe.sage.3@gmail.com>
2023-09-07 21:19:26 +0200

    cart visibitty

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Home from "./pages/home";
 import Navbar from "./components/navbar";
 import Detail from "./pages/detail";
 import { CartProvider } from "./hooks/CartContext";
-
+import { CartVisibilityProvider } from "./hooks/useCartVisibility";
+import CartPage from "./components/CartView";
 function App() {
   return (
-    <CartProvider>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="*" element={<div>No Found Route</div>} />
-      </Routes>
-    </CartProvider>
+    <CartVisibilityProvider>
+      <CartProvider>
+        <CartPage />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="*" element={<div>No Found Route</div>} />
+        </Routes>
+      </CartProvider>
+    </CartVisibilityProvider>
   );
 }
 
